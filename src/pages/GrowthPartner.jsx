879b89f78e0bd07f64cf4905d975873f3b49a6d8
manthan9750 <manthanpilvilkar4444@gmail.com>
2026-06-11 12:14:20 +0530

    Update GrowthPartner.jsx

--- src/pages/GrowthPartner.jsx
+++ src/pages/GrowthPartner.jsx
@@ -174,7 +174,7 @@ export default function GrowthPartner() {
             </p>
 
             <a
-              href="https://wa.me/919999999999"
+              href="https://wa.me/919769710678"
               target="_blank"
               rel="noreferrer"
               className="btn-primary"
